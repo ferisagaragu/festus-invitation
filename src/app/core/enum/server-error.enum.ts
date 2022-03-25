abf47a0bcd087732fd087455d3674549b0a743d2
Fernando Isaías García Aguirre <fernaisagaragu@gmail.com>
2022-03-25 15:57:00 -0600

    🐛 I fixed the orthography correction

--- src/app/core/enum/server-error.enum.ts
+++ src/app/core/enum/server-error.enum.ts
@@ -1,4 +1,4 @@
 export const ServerErrorEnum = {
   unknownError: 'Unknown Error',
-  message: 'Oops parece que hay un error en nuestros servidores, inténtalo mas tarde'
+  message: 'Oops parece que hay un error en nuestros servidores, inténtalo mas tarde.'
 }
