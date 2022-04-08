73dc7cd4ad01e1c363a86255c5424c243a177d02
Fernando Isaías García Aguirre <fernaisagaragu@gmail.com>
2022-04-08 01:04:16 -0500

    I reformat the code form-config-user.component.spec.ts

--- src/app/shared/form-config-user/form-config-user.component.spec.ts
+++ src/app/shared/form-config-user/form-config-user.component.spec.ts
@@ -4,35 +4,52 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { MatDialogModule } from '@angular/material/dialog';
 import { SessionService } from 'ng-urxnium';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { HttpErrorResponse } from '@angular/common/http';
 import { of, throwError } from 'rxjs';
 import { InputUserIconComponent } from '../input-user-icon/input-user-icon.component';
+import { UserService } from '../../core/http/user.service';
 
 class SessionServiceMock {
   getUser() {
     return {
-      "uuid":"2f965d78-dc13-4b66-97b0-2bcd9c13b79d",
-      "name":"Fernando Isaías",
-      "surname":"García",
-      "motherSurname":"Aguirre",
-      "photo":"https://fake",
-      "accountType":"DEFAULT",
-      "userName":"fernnypay95",
-      "email":"ferisagaragu@gmail.com",
-      "createDate":"2022-03-22T21:43:44.580+00:00",
-      "team":null
+      uuid: '2f965d78-dc13-4b66-97b0-2bcd9c13b79d',
+      name: 'Fernando Isaías',
+      surname: 'García',
+      motherSurname: 'Aguirre',
+      photo: 'https://fake',
+      accountType: 'DEFAULT',
+      userName: 'fernnypay95',
+      email: 'ferisagaragu@gmail.com',
+      createDate: '2022-03-22T21:43:44.580+00:00',
+      team: null
     }
   }
 
   signOut() { }
 }
 
+const unauthorizedError = new HttpErrorResponse({
+  error: {
+    message: 'No esta autorizado para realizar esta acción'
+  },
+  status: 401,
+  statusText: 'Unauthorized',
+  url: 'http://fake.com'
+});
+
+const userFormData = {
+  photo: 'http://fake-link',
+  name: 'FakeName',
+  surname: 'FakeSurname',
+  motherSurname: 'FakeMotherSurname'
+}
+
 describe('FormConfigUserComponent', () => {
-  let httpClientSpy: jasmine.SpyObj<HttpClient>;
+  let userServiceSpy: jasmine.SpyObj<UserService>;
   let location = { reload: () => {} };
 
   beforeEach(async () => {
-    httpClientSpy = jasmine.createSpyObj('HttpClient', ['put']);
+    userServiceSpy = jasmine.createSpyObj('UserService', ['updateUser']);
 
     await TestBed.configureTestingModule({
       declarations: [
@@ -49,8 +66,8 @@ describe('FormConfigUserComponent', () => {
           provide: SessionService,
           useValue: new SessionServiceMock()
         },{
-          provide: HttpClient,
-          useValue: httpClientSpy
+          provide: UserService,
+          useValue: userServiceSpy
         },{
           provide: 'location',
           useValue: location
@@ -59,40 +76,34 @@ describe('FormConfigUserComponent', () => {
     }).compileComponents();
   });
 
-  it('should create', () => {
+  it(`should create`, () => {
     const fixture = TestBed.createComponent(FormConfigUserComponent);
     const component = fixture.componentInstance;
     fixture.detectChanges();
     expect(component).toBeTruthy();
   });
 
-  it('when save the form', () => {
+  it(`when save the form`, () => {
     const fixture = TestBed.createComponent(FormConfigUserComponent);
     const component = fixture.componentInstance;
     fixture.detectChanges();
 
-    httpClientSpy.put.and.returnValue(of({ status: 200 }));
-    component.form.reset({
-      photo: 'http://fake-link',
-      name: 'FakeName',
-      surname: 'FakeSurname',
-      motherSurname: 'FakeMotherSurname'
-    });
+    userServiceSpy.updateUser.and.returnValue(of({ status: 200 }));
+    component.form.reset(userFormData);
     fixture.detectChanges();
     component.save();
 
-    expect(httpClientSpy.put.calls.count()).toBe(1);
+    expect(userServiceSpy.updateUser.calls.count()).toBe(1);
   });
 
-  it('when save the form without necessary info', () => {
+  it(`when save the form without necessary info`, () => {
     const fixture = TestBed.createComponent(FormConfigUserComponent);
     const component = fixture.componentInstance;
     fixture.detectChanges();
-    httpClientSpy.put.and.returnValue(of({ status: 200 }));
+
+    userServiceSpy.updateUser.and.returnValue(of({ status: 200 }));
     component.form.reset({
-      photo: 'http://fake-link',
-      name: 'FakeName',
-      surname: 'FakeSurname',
+      ...userFormData,
       motherSurname: null
     });
     fixture.detectChanges();
@@ -101,34 +112,20 @@ describe('FormConfigUserComponent', () => {
     expect(component.form.invalid).toBeTruthy();
   });
 
-  it('when save the form reject service', () => {
+  it(`when save the form reject service`, () => {
     const fixture = TestBed.createComponent(FormConfigUserComponent);
     const component = fixture.componentInstance;
     fixture.detectChanges();
 
-    const error = new HttpErrorResponse({
-      error: {
-        message: 'No esta autorizado para realizar esta acción'
-      },
-      status: 401,
-      statusText: 'Unauthorized',
-      url: 'http://fake.com'
-    });
-    httpClientSpy.put.and.returnValue(throwError(error));
-
-    component.form.reset({
-      photo: 'http://fake-link',
-      name: 'FakeName',
-      surname: 'FakeSurname',
-      motherSurname: 'FakeMotherSurname'
-    });
+    userServiceSpy.updateUser.and.returnValue(throwError(unauthorizedError));
+    component.form.reset(userFormData);
     fixture.detectChanges();
     component.save();
 
-    expect(httpClientSpy.put.calls.count()).toBe(1);
+    expect(userServiceSpy.updateUser.calls.count()).toBe(1);
   });
 
-  it('validate required fields', () => {
+  it(`validate required fields`, () => {
     const fixture = TestBed.createComponent(FormConfigUserComponent);
     const app = fixture.componentInstance;
     fixture.detectChanges();
